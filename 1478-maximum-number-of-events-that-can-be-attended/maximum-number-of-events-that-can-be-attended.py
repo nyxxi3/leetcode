aed23faa9c0548e266db38f5ca12a3bf24324463
Jeevitha R <131524710+nyxxi3@users.noreply.github.com>
2025-06-06 23:07:23 +0530

    Time: 167 ms (37.94%) | Memory: 53.3 MB (29.44%) - LeetSync

---
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.py
@@ -0,0 +1,22 @@
+import heapq
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        events.sort()
+        max_day = max(n[1] for n in events)
+        heap = []
+        e = 0
+        count = 0
+        n = len(events)
+        for i in range(1,max_day+1):
+            while e<n and i== events[e][0]:
+                heapq.heappush(heap,events[e][1])
+                e+=1
+            while heap and heap[0] < i:
+                heapq.heappop(heap)
+            if heap:
+                heapq.heappop(heap)
+                count+=1
+        return count
+                
+
+          
