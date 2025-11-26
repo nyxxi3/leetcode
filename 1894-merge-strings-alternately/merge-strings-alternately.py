f4afd3ca1bfb48e9dbe69a65709f663204e54ae2
Jeevitha R <131524710+nyxxi3@users.noreply.github.com>
2025-11-26 11:42:30 +0530

    Time: 37 ms (62.90%) | Memory: 17.6 MB (94.36%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.py
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -1,23 +1,9 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        #word 1 and word2 as input
-        #assign a var (bool) that switches between word1 and word2 and let it access true-> word1 false -> word2
-        #put it inside loop, let it run n times where n = min(word1, word2), let merged var take in the chars by pop()
-        #end of the loop, look which is empty and add to merged
-        toggle = True
         merged = []
-        word1 = list(word1)
-        word2 = list(word2)  
-        while(word1 and word2):
-            if toggle:
-                merged.append(word1.pop(0))
-                toggle = False
-            else:
-                merged.append(word2.pop(0))
-                toggle = True
-               
-        if word1:
-            merged.extend(word1)
-        else:
-            merged.extend(word2)
+        i = 0
+        while i < len(word1) or i < len(word2):
+            if i < len(word1): merged.append(word1[i])
+            if i < len(word2): merged.append(word2[i])
+            i += 1
         return "".join(merged)
