0fef514dd6ea0dd1dbf35b2cf542722c55c50a60
Jeevitha R <131524710+nyxxi3@users.noreply.github.com>
2025-11-30 17:15:12 +0530

    Time: 111 ms (21.17%) | Memory: 28.6 MB (23.96%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -3,7 +3,6 @@ class Solution:
         left = 0
         right = len(height)-1
         max_area = 0
-        current_area = float('inf')
         while left < right:
             current_area = min(height[left],height[right])*(right-left)
             max_area = max(max_area,current_area)
